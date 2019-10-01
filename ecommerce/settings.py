5fa221c8a4145e86883c85f6c4217f8ca2cebc22
Ubuntu <ubuntu@ip-172-31-32-159.ec2.internal>
2019-10-01 14:12:08 +0000

    updating the django store on my github

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django_forms_bootstrap',
     'accounts',
     'product',
+    'cart',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +66,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.cart_contents'
             ],
         },
     },
@@ -124,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,14 +18,18 @@ from django.contrib import admin
 from accounts.views import index
 from accounts import urls as accounts_urls
 from product import urls as urls_products
+from cart import urls as urls_cart
+from search import urls as urls_search
 from product.views import all_products
 from django.views import static
 from .settings import MEDIA_ROOT
 
 urlpatterns = [
+    url(r'^$', all_products, index, name='index'),
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index, all_products, name="index"),
     url(r'^accounts/', include(accounts_urls)),
     url(r'^product/', include(urls_products)),
-    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
+    url(r'^cart/', include(urls_cart)),
+    url(r'^search/', include(urls_search)),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT})
 ]
