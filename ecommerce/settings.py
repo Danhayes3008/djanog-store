e0836ce5c82f3b5da031431b00b79e33220b4d2e
Ubuntu <ubuntu@ip-172-31-32-159.ec2.internal>
2019-10-04 15:58:52 +0000

    fresh update to keep git log empty

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,7 +40,7 @@ INSTALLED_APPS = [
     'accounts',
     'product',
     'cart',
-    'home',
+    'checkout',
 ]
 
 MIDDLEWARE = [
@@ -133,6 +134,9 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STRIPE_PUBLISHABLE =    os.getenv('STRIPE_PUBLISHABLE')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
