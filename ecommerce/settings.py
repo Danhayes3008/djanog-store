05d0c2f2d8ffcbda90131dca06695c4d5def5c36
Ubuntu <ubuntu@ip-172-31-32-159.ec2.internal>
2019-10-10 11:08:42 +0000

    fixed error with the allowed hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ os.environ.get('ALLOWED_HOSTS'), "https://django-online-store.herokuapp.com/"]
+ALLOWED_HOSTS = [ os.environ.get('ALLOWED_HOSTS'), 'django-online-store.herokuapp.com']
 
 
 # Application definition
